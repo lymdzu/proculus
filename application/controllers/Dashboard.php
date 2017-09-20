95a77d99bb40d90540e4d1145c88078497e21896
liuyongming <liuyongming@shopex.cn>
2017-09-20 08:32:24 +0800

    提交

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 文件名称:Dashboard.php
+ * 摘    要:
+ * 修改日期: 20/9/17
+ */
+class Dashboard extends PublicController
+{
+    public function __construct() {
+        parent::__construct();
+    }
+    public function index()
+    {
+        $this->display("dashboard/index.html");
+    }
+}
