8e25824ad4e361f79a678dd872e374f0a42d26ed
liuyongming <liuyongming@shopex.cn>
2017-11-06 04:04:01 +0800

    食品业

--- application/controllers/Support.php
+++ application/controllers/Support.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 文件名称:Support.php
  * 摘    要:
@@ -6,11 +7,25 @@
  */
 class Support extends DashboardController
 {
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
+
     public function software()
     {
         $this->page("support/software.html");
+    }
+
+    public function tutorials()
+    {
+        $this->vars['page'] = "video";
+        $this->page("support/tutorials.html");
+    }
+
+    public function faqs()
+    {
+        $this->vars['page'] = "faqs";
+        $this->page("support/faqs.html");
     }
 }
